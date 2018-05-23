64f895b2c882a2e0d1f747a12bc7c3e899212446
Ruan Silva <ruan.silva@wideti.com.br>
2018-05-23 16:47:22 -0300

    ajuste aula 04

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,7 +5,8 @@ class DateHelper{
     }
 
     static textoParaData(texto){
-        if (!/\d{4}-\d{2}-\d{2}/.test(texto)){
+        // if (!/\d{4}-\d{2}-\d{2}/.test(texto)){
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto)){
             throw new Error('Deve estar no formato aaaa-mm-dd');
         }
 
